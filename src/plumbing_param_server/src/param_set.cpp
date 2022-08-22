0933cb9659004ceb0fcfb82846c546fd72acbda4
DeathofBrain <l1015178747@outlook.com>
2022-08-22 14:28:32 +0800

    1

--- src/plumbing_param_server/src/param_set.cpp
+++ src/plumbing_param_server/src/param_set.cpp
@@ -2,11 +2,11 @@
 /*
     实现参数的新增或修改
     需求：首先设置机器人的共享参数，类型，半径（0.15m），再修改半径（0.20m）
-    实现L
+    实现
         ros::NodeHandle
-            setParam()
+            setParam("键", 值)
         ros::param
-            set()
+            set("键", 值)
 */ 
 int main(int argc, char *argv[])
 {
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     //方案2：ros::param::set()
     ros::param::set("type_param", "White");
     ros::param::set("radius_param", 0.15);
-    //参数改
+    //参数改(保证键的存在)
     //1. nh.setParam()
     nh.setParam("radius", 0.20);
     //2. ros::param::set()
